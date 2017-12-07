509ac03c8bd106193da5bdba947fdd375a96d282
Yigal Gru <yigalgr@matrix.co.il>
2017-12-07 15:34:52 +0200

    add button

--- aaa/src/app/courses/courses.component.ts
+++ aaa/src/app/courses/courses.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-courses',
-  templateUrl: './courses.component.html',
+  selector: 'course',
+  template: `
+  <h2>compenent: {{ title }} </h2>
+<button class="btn btn-primary">Click me</button>
+
+  `,
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
 
+  
+  title = '  Course Work!';
+
   constructor() { }
 
   ngOnInit() {
+
+
   }
 
 }
