0a853110cc31f8d9e8a49192ad8f5d7b71094460
yigalgru <yigalgru@walla.co.il>
2017-12-11 00:02:21 +0200

    update

--- aaa/src/app/courses/courses.component.ts
+++ aaa/src/app/courses/courses.component.ts
@@ -4,14 +4,45 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-course',
   template: `
   <h2>compenent: {{ title }} </h2>
-<button class="btn btn-primary">Click me</button>
+  <div (click)="onDivClick()">
+<button class="btn btn-primary" [class.active]="isActive"  [style.background-color]="isActive?'blue':'white'"
+(click)="onSave($event)"
+>Click me</button>
 
+</div>
+<input #mail (keyup)="onKeyUp($event)" (keyup.space)="onSpaceUp(mail.value)" />
   `,
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
 
+  isActive = false  ;
+
   title = '  Course Work!';
+
+  onDivClick()
+  {
+    console.log("Div was click");
+  }
+  onSave($event)
+  {
+    console.log("Button was click",$event);
+    $event.stopPropagation();
+    
+  }
+
+  onKeyUp($event)
+  {
+    if ($event.keyCode ==13) {
+      console.log("Enter was press");
+      
+    }
+  }
+
+  onSpaceUp(mail)
+  {
+    console.log("Space was press",mail);   
+  }
 
   constructor() { }
 
