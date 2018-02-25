4339873903ae2f3d272fa497322a438a81321a9c
yigalgru <yigalgru@walla.co.il>
2018-02-25 17:23:49 +0200

    ssss

--- mapbox/src/app/map/map.component.ts
+++ mapbox/src/app/map/map.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'mapbox.js';
 
 @Component({
-  selector: 'app-map',
+  selector: 'map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
+
+
 export class MapComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    L.mapbox.accessToken = 'pk.eyJ1IjoieWlnYWxncnUiLCJhIjoiY2piM2VlNmZ3MmtrNjJ3cWs1NXViaW1wdiJ9.QQYcKh8R86lBXINgnnW1uQ';
+    var map = L.mapbox.map('map', 'mapbox.dc-markers');
+
+    map.featureLayer.on('click', function(e) {
+        map.panTo(e.layer.getLatLng());
+    });
   }
 
 }
