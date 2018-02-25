d3c27e439ecc7fde445c24844550e8353976eae6
yigalgru <yigalgru@walla.co.il>
2018-02-25 10:46:24 +0200

    wwww

--- leafletApp/src/app/my-map/my-map.component.ts
+++ leafletApp/src/app/my-map/my-map.component.ts
@@ -3,6 +3,8 @@ import * as mapboxgl from 'mapbox-gl';
 import { MapService } from '../map.service';
 import { GeoJson, FeatureCollection } from '../map';
 
+
+
 @Component({
   selector: 'my-map',
   templateUrl: './my-map.component.html',
@@ -32,9 +34,9 @@ export class MyMapComponent implements OnInit {
     mapboxgl.accessToken = 'pk.eyJ1IjoieWlnYWxncnUiLCJhIjoiY2piM2VlNmZ3MmtrNjJ3cWs1NXViaW1wdiJ9.QQYcKh8R86lBXINgnnW1uQ';
     var map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/satellite-v9',
-      center: [35.34, 31.907],
-      zoom: 6
+       style: "mapbox://styles/mapbox/outdoors-v9",
+      center: [-121.403732, 40.492392],
+      zoom: 10
     });
 
     map.on('load', function () {
@@ -77,8 +79,6 @@ export class MyMapComponent implements OnInit {
       });
     });
 
-
-
     map.on('load', function() {
       map.loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Cat_silhouette.svg/400px-Cat_silhouette.svg.png', function(error, image) {
           if (error) throw error;
@@ -105,7 +105,92 @@ export class MyMapComponent implements OnInit {
               }
           });
       });
+
+
+
+      
   });
+
+
+//new code
+
+map.on("load", function() {
+  map.addSource("national-park", {
+      "type": "geojson",
+      "data": {
+          "type": "FeatureCollection",
+          "features": [{
+              "type": "Feature",
+              "geometry": {
+                  "type": "Polygon",
+                  "coordinates": [
+                      [
+                          [-121.353637, 40.584978],
+                          [-121.284551, 40.584758],
+                          [-121.275349, 40.541646],
+                          [-121.246768, 40.541017],
+                          [-121.251343, 40.423383],
+                          [-121.326870, 40.423768],
+                          [-121.360619, 40.434790],
+                          [-121.363694, 40.409124],
+                          [-121.439713, 40.409197],
+                          [-121.439711, 40.423791],
+                          [-121.572133, 40.423548],
+                          [-121.577415, 40.550766],
+                          [-121.539486, 40.558107],
+                          [-121.520284, 40.572459],
+                          [-121.487219, 40.550822],
+                          [-121.446951, 40.563190],
+                          [-121.370644, 40.563267],
+                          [-121.353637, 42.584978]
+                      ]
+                  ]
+              }
+          }, {
+              "type": "Feature",
+              "geometry": {
+                  "type": "Point",
+                  "coordinates": [-121.415061, 40.506229]
+              }
+          }, {
+              "type": "Feature",
+              "geometry": {
+                  "type": "Point",
+                  "coordinates": [-121.505184, 40.488084]
+              }
+          }, {
+              "type": "Feature",
+              "geometry": {
+                  "type": "Point",
+                  "coordinates": [-121.354465, 40.488737]
+              }
+          }]
+      }
+  });
+
+    map.addLayer({
+        "id": "park-boundary",
+        "type": "fill",
+        "source": "national-park",
+        "paint": {
+            "fill-color": "green",
+            "fill-opacity": 0.4
+        },
+        "filter": ["==", "$type", "Polygon"]
+    });
+
+    map.addLayer({
+        "id": "park-volcanoes",
+        "type": "circle",
+        "source": "national-park",
+        "paint": {
+            "circle-radius": 6,
+            "circle-color": "#B42222"
+        },
+        "filter": ["==", "$type", "Point"],
+    });
+});
+  
 
   }
   private initializeMap() {
